8926f10ea27b03310153d31400a4e48377fed2e5
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:07:23 -0700

    chore: enforce ultracite

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,12 @@
-import {expectType, expectError} from 'tsd';
-import offloadFunction from './index.js';
+import { expectError, expectType } from "tsd";
+import offloadFunction from "./index.js";
 
 expectType<Promise<number>>(offloadFunction(() => 42));
-expectType<Promise<string>>(offloadFunction((name: string) => `hello ${name}`, 'world'));
-expectType<Promise<number>>(offloadFunction((a: number, b: number) => a + b, 1, 2));
+expectType<Promise<string>>(
+  offloadFunction((name: string) => `hello ${name}`, "world")
+);
+expectType<Promise<number>>(
+  offloadFunction((a: number, b: number) => a + b, 1, 2)
+);
 
 expectError(offloadFunction());
